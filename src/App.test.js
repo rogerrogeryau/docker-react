88a4f4968bdb8101fd3184b6817a99d170813ec8
root <root@DESKTOP-QHK5NGQ.localdomain>
2019-06-16 13:13:43 +0800

    Fourth Trail to fix [Travis CI gets stuck on 'ran all test suites']

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
+}); 
 
 // it('renders without crashing', () => {
 //   const div = document.createElement('div');
